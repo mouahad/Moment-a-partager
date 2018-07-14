deca63ea9dd5e0c7649e3c9968e06fddc2749582
MYD <myd@macbook-air-de-myd.home>
2018-07-14 15:29:19 +0200

    premier commit

---
+++ Partager les moments/PhotoController.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoController.swift
+//  Partager les moments
+//
+//  Created by MYD on 14/07/2018.
+//  Copyright © 2018 MYD. All rights reserved.
+//
+
+import UIKit
+
+class PhotoController: UIViewController {
+    @IBOutlet weak var partagerBouton: UIBarButtonItem!
+    
+    @IBOutlet weak var photoAPartager: UIImageView!
+    @IBOutlet weak var texteAPartager: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func boutonPartager(_ sender: Any) {
+    }
+    
+}
